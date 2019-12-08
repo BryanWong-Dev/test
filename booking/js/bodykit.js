9b29cc3aa1cdb387d3d83c4407597023b5d697f9
BryanWong-Dev <zilam0429@gmail.com>
2019-12-09 00:13:46 +0800

    first commit

---
+++ booking/js/bodykit.js
@@ -0,0 +1,89 @@
+function circle1() {
+    var x = document.getElementById("dcircle1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function circle2() {
+    var x = document.getElementById("dcircle2");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function circle3() {
+    var x = document.getElementById("dcircle3");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function circle4() {
+    var x = document.getElementById("dcircle4");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function circle5() {
+    var x = document.getElementById("dcircle5");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function circle6() {
+    var x = document.getElementById("dcircle6");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function circle7() {
+    var x = document.getElementById("dcircle7");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function circle8() {
+    var x = document.getElementById("dcircle8");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function circle9() {
+    var x = document.getElementById("dcircle9");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function circle10() {
+    var x = document.getElementById("dcircle10");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
